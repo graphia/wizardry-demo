f111a2cfb5a0a93720cbe2abb7c595777b8d0f7a
Peter Yates <peter.yates@graphia.co.uk>
2021-11-07 15:33:35 +0000

    Add wizardry to order controller and set up wizard

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,57 @@
 class OrdersController < ApplicationController
+  include Wizardry
+
+  wizard(
+    name: 'orders',
+    class_name: 'Order',
+    edit_path_helper: :orders_edit_path,
+    update_path_helper: :orders_update_path,
+    pages: [
+      Wizardry::Pages::QuestionPage.new(
+        :who_are_you,
+        title: 'Who are you?',
+        questions: [
+          Wizardry::Questions::ShortAnswer.new(:full_name),
+          Wizardry::Questions::ShortAnswer.new(:name)
+        ]
+      ),
+      Wizardry::Pages::QuestionPage.new(
+        :size,
+        title: 'What size do you want?',
+        questions: [
+          Wizardry::Questions::Radios.new(:size, { 9 => %(9 inches), 12 => %(12 inches), 15 => %(15 inches) })
+        ],
+        pages: [
+          Wizardry::Pages::QuestionPage.new(
+            :bonus_sides,
+            title: %(You've qualified for a bonus side dish),
+            questions: [
+              Wizardry::Questions::Radios.new(:bonus_side_dish, { wedges: "Wedges", fries: "Fries", salad: "Salad" })
+            ]
+          )
+        ],
+        next_pages: [
+          Wizardry::Routing::NextPage.new(:bonus_sides, proc { |o| o.size == 15 }, label: %(Was the size 15 inches?))
+        ]
+      ),
+      Wizardry::Pages::QuestionPage.new(
+        :topping,
+        title: 'Which topping would you like?',
+        questions: [
+          Wizardry::Questions::Radios.new(
+            :topping,
+            {
+              anchovies: 'Anchovies',
+              four_seasons: 'Four seasons',
+              hawaiian: 'Hawaiian'
+            }
+          )
+        ]
+      ),
+      Wizardry::Pages::CheckYourAnswersPage.new(
+        questions: [Wizardry::Questions::Hidden.new(:complete, true)]
+      ),
+      Wizardry::Pages::CompletionPage.new
+    ]
+  )
 end
