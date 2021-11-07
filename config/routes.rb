305c55448ef669515d55f5594df647ea45500e5b
Peter Yates <peter.yates@graphia.co.uk>
2021-11-07 15:32:49 +0000

    Add routes for our orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resource "hello", only: :show
 
   root to: "hello#show"
+
+  get   %(order/:page), to: %(orders#edit),   as: :orders_edit
+  patch %(order/:page), to: %(orders#update), as: :orders_update
 end
